214dbba5f9fc81b33a69e145ca04ca23ca5c2633
Artsem Hluzd <artsem.hluzd@gmail.com>
2020-06-03 21:20:46 +0300

    refactor eslint stuff

--- js/stat.js
+++ js/stat.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_START_X = 100;
@@ -11,35 +13,35 @@ var DISTANCE_BETWEEN_HISTOGRAM = 50;
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
-}
+};
 
 var renderText = function (ctx, x, y, index, text, color) {
   ctx.fillStyle = color;
   ctx.fillText(text, x, y + 20);
-}
+};
 
 var renderTime = function (ctx, x, y, index, time, color, height) {
   ctx.fillStyle = color;
-    ctx.fillText(time, x, y - height - 5);
-}
+  ctx.fillText(time, x, y - height - 5);
+};
 
 var renderBar = function (ctx, x, y, index, height, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y - height, DISTANCE_BETWEEN_HISTOGRAM, height);
-}
+};
 
-var getRandomInt = function(min, max) {
+var getRandomInt = function (min, max) {
   return Math.round(Math.random() * (max - min) + 1);
-}
+};
 
 var randomBlue;
-var getRandomBlue = function() {
+var getRandomBlue = function () {
   var hue = 245;
   var suration = getRandomInt(1, 100);
   var lightness = 50;
-  randomBlue = "hsl(" + hue + ", " + suration + "%, " + lightness + "%)";
+  randomBlue = 'hsl(' + hue + ', ' + suration + '%, ' + lightness + '%)';
   return randomBlue;
-}
+};
 
 window.renderStatistics = function (ctx, arrName, arrTime) {
 
@@ -58,23 +60,23 @@ window.renderStatistics = function (ctx, arrName, arrTime) {
   var findMaxTime = function (arr) {
     maxTime = Math.max.apply(null, arr);
     return Math.round(maxTime);
-  }
+  };
   findMaxTime(arrTime);
 
-  var renderHistogram = function (ctx, index, name, colorText, colorHistogram, time, height) {
+  var renderHistogram = function (context, index, name, colorText, colorHistogram, time, height1) {
     var x = HISTOGRAM_START_X + (WEIGHT_HISTOGRAM * index) + (DISTANCE_BETWEEN_HISTOGRAM * index);
     var y = HISTOGRAM_START_Y;
-    renderText(ctx, x, y, index, name, colorText);
-    renderTime(ctx, x, y, index, time, colorText, height);
-    renderBar(ctx, x, y, index, height, colorHistogram);
-  }
+    renderText(context, x, y, index, name, colorText);
+    renderTime(context, x, y, index, time, colorText, height1);
+    renderBar(context, x, y, index, height, colorHistogram);
+  };
 
   // Вывод гистограмм
   for (var i = 0; i < arrTime.length; i++) {
-    var height = (arrTime[i] * MAX_HEIGHT_HISTOGRAM) / maxTime;
+    height = (arrTime[i] * MAX_HEIGHT_HISTOGRAM) / maxTime;
     var name = arrName[i];
     var time = arrTime[i];
-    if (arrName[i] == "Вы") {
+    if (arrName[i] === 'Вы') {
       renderHistogram(ctx, i, name, 'black', 'rgba(255, 0, 0, 1)', Math.round(time), height);
     } else {
       getRandomInt(1, 100);
@@ -82,4 +84,4 @@ window.renderStatistics = function (ctx, arrName, arrTime) {
       renderHistogram(ctx, i, name, 'black', randomBlue, Math.round(time), height);
     }
   }
-}
+};
