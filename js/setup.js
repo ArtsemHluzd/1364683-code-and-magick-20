91967b10eafa9b902cb1e585af88f563944433ee
Artsem Hluzd <artsem.hluzd@gmail.com>
2020-06-04 23:49:08 +0300

    create generateObj instead of fillCharacterObj

--- js/setup.js
+++ js/setup.js
@@ -1,5 +1,11 @@
 'use strict';
 
+var characters = [];
+var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var surnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var coatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var eyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
+
 var setup = document.querySelector('.setup');
 setup.classList.remove('hidden');
 
@@ -7,27 +13,25 @@ var getRandomInt = function (min, max) {
   return Math.round(Math.random() * (max - min) + 1);
 };
 
-var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var surnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var coatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-
 var generateElement = function (element) {
   var random = getRandomInt(0, element.length - 2);
   return element[random];
 }
 
-var fillCharacterObj = function (nameArr, surnameArr, coatColorArr) {
-  for (var i = 0; i < nameArr.length; i++) {
-  // var random = getRandomInt(0, nameArr.length - 2);
-  // var name = nameArr[random];
-  var name = generateElement(nameArr) + ' ' + generateElement(surnameArr);
-  var coatColor = generateElement(coatColorArr);
+var generateObj = function (nameArr, surnameArr, coatColorArr, eyesColorArr) {
 
-  console.log(name + coatColor);
-}
+  for (var i = 0; i < nameArr.length; i++) {
+
+    var name = generateElement(nameArr) + ' ' + generateElement(surnameArr);
+    var coatColor = generateElement(coatColorArr);
+    var eyesColor = generateElement(eyesColorArr);
+
+    characters[i] = {
+      name: name,
+      coatColor: coatColor,
+      eyesColor: eyesColor
+    }
+  }
 };
 
-fillCharacterObj(names, surnames, coatColor);
-  //   CSSConditionRule.log
-  // }
-// }
+generateObj(names, surnames, coatColor, eyesColor);
