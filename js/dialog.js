cfdd62e46d9b1afb9889d5b2078157916e8ff706
Artsem Hluzd <artsem.hluzd@gmail.com>
2020-06-21 11:20:31 +0300

    finish dialog.js

--- js/dialog.js
+++ js/dialog.js
@@ -2,18 +2,36 @@
 
 (function () {
   var userDialog = window.setup.userDialog;
-  console.log(userDialog);
+  var userDialogUpload = userDialog.querySelector('.upload');
 
-  userDialog.addEventListener('mousedown', function () {
-    console.log(userDialog);
-  });
 
-  userDialog.addEventListener('mousemove', function () {
+  userDialogUpload.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
 
-  });
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
 
-  userDialog.addEventListener('mouseup', function () {
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
 
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        Y: startCoords.y - moveEvt.clientY
+      };
+
+      userDialogUpload.style.top = (userDialog.offsetTop - shift.y) + 'px';
+      userDialogUpload.style.left = (userDialog.offsetLeft - shift.x) + 'px';
+    };
+
+    var onMouseUp = function (upEvt) {
+      document.removeEventListener(onMouseMove);
+      document.removeEventListener(onMouseUp);
+    };
+
+    userDialogUpload.addEventListener('mousemove', onMouseMove);
+    userDialogUpload.addEventListener('mouseup', onMouseUp);
   });
 
 })();
